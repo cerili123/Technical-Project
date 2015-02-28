68c9a6b785053b9c35d626e0c68245b1b08b8dcd
Cerian Lindsay <Cerian@Cerians-MacBook.local>
2015-02-28 14:04:25 +0000

    no message

--- BowlingGame.java
+++ BowlingGame.java
@@ -11,11 +11,40 @@ import java.io.*;
  */
 	 
 public class BowlingGame {
-	
-	
 
-	public static void main(String[] args){
-		
-	}
+
+	public static void main(String[] args) {
+	
+	
+		System.out.println("Welcome to Bowling Heaven!");
+	
+		//Prompt the user to enter the number of players
+		System.out.println("Please enter the number of bowlers: ");
+	
+		//Open up standard input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int numberOfBowlers = 0;
+	
+		// Read the number of bowlers from the command line; 
+    	try {
+
+    		if (0 < Integer.parseInt("br.read()") && Integer.parseInt("br.read()") < 7) {
+        		numberOfBowlers = Integer.parseInt("br.read()");
+        		System.out.println("Great, " + numberOfBowlers + " it is!");
+    		}
+    		else {
+    			System.out.println("This is not a valid format. Please enter a number between 1 and 6");
+        		System.exit(1);
+   			}
+   		}
+   		catch (NumberFormatException e) {
+   			System.out.println(e.getMessage());
+  	 	}
+//   	catch (IOException e) {
+  // 		System.out.println(e.getMessage());
+ 		//}
+
+	} //end of main
     
 } // end of BowlingGame class
