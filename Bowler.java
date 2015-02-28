466c5aeb3260a69026ec8bcb451a868565d38e61
Cerian Lindsay <Cerian@Cerians-MacBook.local>
2015-02-28 12:04:21 +0000

    no message

--- Bowler.java
+++ Bowler.java
@@ -1,5 +1,6 @@
 /*
- * 
+ * BBC technical project
+ * Cerian Lindsay 2015
  */
 
 import java.util.*; 
@@ -12,15 +13,27 @@ import java.io.*;
 
 public class Bowler {
 
-	private String bowlerName;
+	//
+	private String name;
+	
+	//Define an array of integers to store the scores in a bowlers game.
+	//A game consists of 10 frames, each of two rolls.
+	//A bowler earns a bonus roll if they score a strike in the final frame.
+	private int[] rolls = new int[21];
+	
+	//
+	int score;
 	
 	
 	/**
 	 * 
 	 */
 	
-	public Bowler(String bowlerName) {
-		bowlerName = this.bowlerName;
+	public Bowler(String name) {
+		name = this.name;
+		rolls = this.rolls;
+		score = 0;
+		
 	} 
 	
 	
@@ -28,18 +41,55 @@ public class Bowler {
 	 *
 	 */
 	 
-	 public String getBowlerName() {
-	 	return bowlerName;
-	 }
+//	 public String getName() {
+//	 	return name;
+//	 }
 	 
 	 
 	/**
-	 * Updates the bowlers name
+	 * Updates the bowlers name - not important?
 	 */
 	  
-    public void setBowlerName(String newBowlerName) {
-        bowlerName = newBowlerName;
+//    public void setBowlerName(String newBowlerName) {
+ //       bowlerName = newBowlerName;
+//    }
+    
+    
+	/**
+	 * A method to store the score from a bowlers game,
+	 * specifying a strike, spare and bonus ...
+	 */
+	 
+	public void setScore() /* throws exception*/ {
+		
+		for (int j = 0; j < 20; j = j+2) {
+			
+			if (rolls[j] == 10) {
+				//strike!
+				score += rolls[j] + rolls[j+1] + rolls[j+2];
+			}
+			
+			else if (rolls[j] + rolls[j+1] == 10) {
+				//spare!
+				score += rolls[j] + rolls[j+1] + rolls[j+2];
+			}
+			
+			else if (rolls[j] + rolls[j+1] < 10 ) {
+				score += rolls[j] + rolls[j+1];
+			}
+			
+		}
+		
+	} //end of score() method
+	
+	
+	/**
+	 *
+	 */
+	    
+    public int[] getRolls() {
+    	return rolls;
     }
-	  
+     
 	
 } // end of Bowler class
---
+++ BowlingGame.java
@@ -0,0 +1,21 @@
+/*
+ * BBC technical project
+ * Cerian Lindsay 2015
+ */
+ 
+import java.util.*; 
+import java.io.*;
+
+/**
+ * A class to calculate the score of a ten pin bowling game for up to 6 bowlers
+ */
+	 
+public class BowlingGame {
+	
+	
+
+	public static void main(String[] args){
+		
+	}
+    
+} // end of BowlingGame class
